4048a14fee606be6f0e85ec173141cc3ea2dcdee
shilpasudhish <shilpasudhish@gmail.com>
2025-02-13 23:22:18 +0530

    minor fix

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Event from './Event';
 
-const EventList = ( { events = []}) => {
+const EventList = ( { events }) => {
   return (
     <ul id="event-list" role="list">
         {events ? events.map(event => <Event event={event} key={event.id} />) : null}
