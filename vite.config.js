2ef007c22e8aca14bd3688ebe8a5586db7bc33f3
shilpasudhish <shilpasudhish@gmail.com>
2025-02-16 00:42:43 +0530

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -8,39 +8,39 @@ export default defineConfig({
    react(),
    VitePWA({
      manifest: {
-       "short_name": "React App",
-       "name": "Create React App Sample",
-       "icons": [
-           {
-           "src": "favicon.ico",
-           "sizes": "48x48",
-           "type": "image/x-icon",
-           "purpose": "maskable"
-           },
-           {
-           "src": "meet-app-144.png",
-           "type": "image/png",
-           "sizes": "144x144",
-           "purpose": "any"
-           },
-           {
-           "src": "meet-app-192.png",
-           "type": "image/png",
-           "sizes": "192x192",
-           "purpose": "maskable"
-           },
-           {
-           "src": "meet-app-512.png",
-           "type": "image/png",
-           "sizes": "512x512",
-           "purpose": "maskable"
-           }
-       ],
-       "start_url": ".",
-       "display": "standalone",
-       "theme_color": "#000000",
-       "background_color": "#ffffff"
-     },
+      "short_name": "Meet App",
+      "name": "Meet App",
+      "icons": [
+          {
+          "src": "favicon.ico",
+          "sizes": "48x48",
+          "type": "image/x-icon",
+          "purpose": "maskable"
+          },
+          {
+          "src": "meet-app-144.png",
+          "type": "image/png",
+          "sizes": "144x144",
+          "purpose": "any"
+          },
+          {
+          "src": "meet-app-192.png",
+          "type": "image/png",
+          "sizes": "192x192",
+          "purpose": "maskable"
+          },
+          {
+          "src": "meet-app-512.png",
+          "type": "image/png",
+          "sizes": "512x512",
+          "purpose": "maskable"
+          }
+      ],
+      "start_url": ".",
+      "display": "standalone",
+      "theme_color": "#000000",
+      "background_color": "#ffffff"
+   },
      srcDir: 'src', // Update if your service-worker.js is elsewhere
      filename: 'service-worker.js', // Ensure it's accessible in production
      registerType: 'autoUpdate',
